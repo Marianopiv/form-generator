89a88cb809e9ae586bbaa59ea7b69016c11d8935
Marianopiv <marianopividori93@gmail.com>
2023-01-04 00:02:53 -0300

    subo el contenedor

--- src/components/listedInputs/ListedInputs.jsx
+++ src/components/listedInputs/ListedInputs.jsx
@@ -9,10 +9,10 @@ const ListedInputs = ({inputs,handleValue, FinalRecolect, capturedValue,recolect
     const { ChangeShow,show } = useShow();
   return (
     <>
-    {show?<><div className='absolute top-0 right-0 bottom-0 left-0 m-auto rounded-md w-2/3 h-2/3 z-50 bg-cyan-500 p-4 shadow-2xl'><h1 className='uppercase font-bold'>{title}</h1>{inputs.map((item, index) => (<>
+    {show?<><div className='absolute top-0 right-0 bottom-32 left-0 m-auto rounded-md w-2/3 h-2/3 z-50 bg-cyan-500 p-4 shadow-2xl'><FaWindowClose onClick={ChangeShow} className='mx-auto mb-10 hover:cursor-pointer text-2xl'/><h1 className='uppercase font-bold'>{title}</h1>{inputs.map((item, index) => (<>
       
        <Input type={item.type} span={item.span} key={index} handleValue={handleValue} FinalRecolect={FinalRecolect} capturedValue={capturedValue} recolected={recolected}/></>
-      ))} <button  className='bg-blue-800 mt-4 ml-4' onClick={()=>generarJson(capturedValue)}>Encrypt File</button><button onClick={()=>downloadTxt(jwt,"form")} className='bg-blue-800  mt-4 ml-4'>Download Encrypted</button><FaWindowClose onClick={ChangeShow} className='mx-auto mt-2 hover:cursor-pointer text-2xl'/></div></>:<FaFolderOpen className='mx-auto mt-2 hover:cursor-pointer text-2xl' onClick={ChangeShow}/>}
+      ))} <button  className='bg-blue-800 mt-4 ml-4' onClick={()=>generarJson(capturedValue)}>Encrypt File</button><button onClick={()=>downloadTxt(jwt,"form")} className='bg-blue-800  mt-4 ml-4'>Download Encrypted</button></div></>:<FaFolderOpen className='mx-auto mt-2 hover:cursor-pointer text-2xl' onClick={ChangeShow}/>}
     </>
   )
 }
